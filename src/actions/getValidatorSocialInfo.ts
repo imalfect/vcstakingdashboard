036f9e59bbc23f35560088117b28c3a9307a6863
iMalFect <me@imal.dev>
2024-06-28 14:57:39 +0200

    switch destr to JSON.parse

--- src/actions/getValidatorSocialInfo.ts
+++ src/actions/getValidatorSocialInfo.ts
@@ -5,10 +5,14 @@ import ValidatorSocialInfo from '@/types/socialInfo';
 import { ofetch } from 'ofetch';
 
 export default async function getValidatorSocialInfo(url: string) {
-	const data = await ofetch<ValidatorSocialInfo>(url).catch((e) => {
+	const data = await ofetch<ValidatorSocialInfo>(url, {
+		parseResponse: JSON.parse
+	}).catch((e) => {
 		return {
 			error: e.response?.status === 404 ? 'not-found' : 'cors'
 		} as ValidatorSocialInfo;
 	});
+	console.log('Fetched social info for', url, data?.name || 'Unknown Validator');
+	console.log(data);
 	return data;
 }
